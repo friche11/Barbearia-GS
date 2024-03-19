817cfcfec1bfbbadc1fa711f54d2dbdb12889a89
friche11 <vicfriche@gmail.com>
2024-03-19 01:02:24 -0300

    Model de cliente

---
+++ src/main/java/com/web/BarbeariaGS/services/ClientesRepo.java
@@ -0,0 +1,11 @@
+package com.web.BarbeariaGS.services;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.web.BarbeariaGS.models.Cliente;
+
+public interface ClientesRepo extends CrudRepository<Cliente, Integer>{
+    
+   
+
+}
