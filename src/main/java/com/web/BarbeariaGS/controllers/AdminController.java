c9c6cd72ebdd8c9cc5e320de1449789d763f3e03
friche11 <vicfriche@gmail.com>
2024-03-19 18:31:26 -0300

    Erro de email já cadastrado para admin

--- src/main/java/com/web/BarbeariaGS/controllers/AdminController.java
+++ src/main/java/com/web/BarbeariaGS/controllers/AdminController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.web.BarbeariaGS.models.Admin;
 import com.web.BarbeariaGS.services.AdminRepo;
@@ -28,7 +29,14 @@ public class AdminController {
 
     //Rota para metodo POST de cadastro de admin
     @PostMapping("/administradores/criar")
-    public String criar(Admin admin){
+    public String criar(Admin admin, @RequestParam String email){
+        // Verifica se o e-mail já está em uso
+        if (repo.existsByEmail(email)) {
+            // Se o e-mail já está em uso, redireciona de volta para a página de cadastro com uma mensagem de erro
+            return "redirect:/administradores/novo?error=emailInUse";
+        }
+        
+        // Se o e-mail não está em uso, salva o admin e redireciona para a página de admin
         repo.save(admin);
         return "redirect:/administradores";
     }
--- src/main/java/com/web/BarbeariaGS/services/AdminRepo.java
+++ src/main/java/com/web/BarbeariaGS/services/AdminRepo.java
@@ -10,4 +10,7 @@ public interface AdminRepo extends CrudRepository<Admin, Integer>{
     @Query(value= "select CASE WHEN count(1)>0 THEN 'true' ELSE 'false' END from administradores where id = :id", nativeQuery = true)
     public boolean exist(int id);
 
+    @Query(value= "select CASE WHEN count(1)>0 THEN 'true' ELSE 'false' END from administradores where email = :email", nativeQuery = true)
+    public boolean existsByEmail(String email);
+
 }
