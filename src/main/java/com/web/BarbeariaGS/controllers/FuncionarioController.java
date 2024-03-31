08f967dfea38459587ce5213f19942b758812ebe
friche11 <vicfriche@gmail.com>
2024-03-31 18:13:13 -0300

    Model de funcionario

---
+++ src/main/java/com/web/BarbeariaGS/controllers/FuncionarioController.java
@@ -0,0 +1,5 @@
+package com.web.BarbeariaGS.controllers;
+
+public class FuncionarioController {
+    
+}
---
+++ src/main/java/com/web/BarbeariaGS/models/Funcionario.java
@@ -0,0 +1,70 @@
+package com.web.BarbeariaGS.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "funcionarios")
+public class Funcionario {
+     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+     private int id;
+
+     @Column(name = "nome", length = 100, nullable = false)
+     private String nome;
+
+     @Column(name = "cargo", length = 100, nullable = false)
+     private String cargo;
+ 
+     @Column(name = "email", length = 256, nullable = false)
+     private String email;
+ 
+     @Column(name = "senha", length = 10, nullable = false)
+     private String senha;
+    
+     public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+}
