01f1f34e03f5e43f9b98e687fcc169e7bbd24374
friche11 <vicfriche@gmail.com>
2024-04-06 19:11:11 -0300

    AuthService

---
+++ src/main/java/com/web/BarbeariaGS/services/AuthService/LoginInterceptorAppConfig.java
@@ -0,0 +1,26 @@
+package com.web.BarbeariaGS.services.AuthService;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class LoginInterceptorAppConfig implements WebMvcConfigurer  {
+
+    // Intercepta todas as requsicoes, menos as excluidas
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+            .excludePathPatterns(
+                "/login",
+                "/error",
+                "/logar",
+                "/js/**",
+                "/css/**",
+                "/img/**",
+                "/"
+                
+            );
+    }
+    
+}
